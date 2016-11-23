12f17a224e4f6a34dcebfcac6697a1893d7391df
Simon Felix <simonf7070@gmail.com>
2016-11-23 08:28:53 +0000

    Add translation test

---
+++ TennisScoring.Tests/GameScorerI18NTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Threading;
+using NUnit.Framework;
+using TennisScoring.Game;
+
+namespace TennisScoring.Tests
+{
+    [TestFixture]
+    public class GameScorerI18NTests
+    {
+        [Test]
+        public void PlayMatchWithWithCultureUIFrench_CurrentScoreShouldBeCorrect()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
+            var gameScorer = new Scorer();
+            Assert.That(gameScorer.ToString(), Is.EqualTo("Aimer tout"));
+        }
+    }
+}
